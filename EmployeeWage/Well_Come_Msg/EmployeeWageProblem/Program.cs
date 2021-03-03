3242130bae0483e895bd9a95a01e09ef6140140d
Kajal <kajalpatil158@gmail.com>
2021-03-03 17:31:57 +0530

    [Kajal]ADD: Total_Working_Hours_Days_Reach_For_Month

--- EmployeeWage/Well_Come_Msg/EmployeeWageProblem/Program.cs
+++ EmployeeWage/Well_Come_Msg/EmployeeWageProblem/Program.cs
@@ -1,42 +1,23 @@
 ﻿using System;
 
-namespace Calculate_Wages_For_Month
+namespace Calculate_Wages_Till_Moth
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int salary;
-            //int Is_Full_Time = 1;
-            //int Is_Part_Time = 2;
-            Random random = new Random();
-            int EmpCheck = random.Next(0, 3);
-            int Working_Day = 20;
-            int EmpHr = 8;
-            int i;
-            //int EmpCheck = random.Next(0, 3);
-            //for (i=1;i<=Working_Day;i++)
-            if (EmpCheck == 1)
-            {
+            int Total_Working_hr = 100;
+            int Total_Working_day = 20;
+            int j;
+            int wages = 0;
+            //for (i = 1; i <= Total_Working_day; i++)
+            //{
+            for (j = 1; j <= Total_Working_hr; j++)
 
-                EmpHr = 8;
-                Console.WriteLine("Employee is full time");
+                wages = Total_Working_day * Total_Working_hr;
+            Console.WriteLine("Total Working Wages For Month is = {0}", wages);
 
-            }
-            else if (EmpCheck == 2)
-            {
-
-                EmpHr = 4;
-                Console.WriteLine("Employee is Part time");
-
-            }
-            else
-            {
-                EmpHr = 0;
-                Console.WriteLine("Employee is Absent:");
-            }
-            salary = EmpHr * Working_Day;
-            Console.WriteLine("Employee Working salary for month is {0}", salary);
+            //}
         }
     }
 }
