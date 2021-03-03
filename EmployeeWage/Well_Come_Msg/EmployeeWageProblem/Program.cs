e013d93b145e9cdaede9dc308f2303d1112f36ad
Kajal <kajalpatil158@gmail.com>
2021-03-03 16:42:37 +0530

    [Kajal]ADD: Employee_Present_Or_Absent

--- EmployeeWage/Well_Come_Msg/EmployeeWageProblem/Program.cs
+++ EmployeeWage/Well_Come_Msg/EmployeeWageProblem/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace EmployeeWageProblem
+namespace EmployeeAttaince
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Well Come In Employee Wage:");
+            int Is_Full_Time = 1;
+            Random random = new Random();
+            int EmpCheck = random.Next(0, 2);
+            if (EmpCheck == 1)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
