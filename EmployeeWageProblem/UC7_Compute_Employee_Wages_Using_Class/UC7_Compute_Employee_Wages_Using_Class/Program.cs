4e5f9d1878ec55fe175c668fadb765b1cd4559c0
Kajal <kajalpatil158@gmail.com>
2021-03-05 22:31:03 +0530

    [Kajal]ADD: Wage_For_Multiple_Company

--- EmployeeWageProblem/UC7_Compute_Employee_Wages_Using_Class/UC7_Compute_Employee_Wages_Using_Class/Program.cs
+++ EmployeeWageProblem/UC7_Compute_Employee_Wages_Using_Class/UC7_Compute_Employee_Wages_Using_Class/Program.cs
@@ -6,9 +6,9 @@ namespace UC7_Compute_Employee_Wages_Using_Class
     {
         public const int Is_Part_Time = 1;
         public const int Is_Full_Time = 2;
-        public const int Max_Working_Days = 20;
-        public const int Working_Hr = 100;
-        public static void Compute_Emp_Wages_Using_Class()
+        //public const int Max_Working_Days = 20;
+        //public const int Working_Hr = 100;
+        public static int Compute_Emp_Wages_Using_Class(string Company,int Emp_Rate_Per_Hr,int Max_Working_Days,int Working_Hr)
         {
             int Emp_Hr = 0;
             int TotalWorkingHr = 0;
@@ -18,6 +18,7 @@ namespace UC7_Compute_Employee_Wages_Using_Class
                 TotalWorkingDays++;
                 Random random = new Random();
                 int EmpCheck = random.Next(0, 3);
+                //int Emp_Hr = 0;
                 switch (EmpCheck)
                 {
                     case Is_Part_Time:
@@ -31,18 +32,22 @@ namespace UC7_Compute_Employee_Wages_Using_Class
                         break;
                 }
 
-                int Total_Emp_Hr = Emp_Hr;
+                int Total_Emp_Hr =+ Emp_Hr;
                 Console.WriteLine("Total Employee Wage For Employee Is: " + Total_Emp_Hr);
             }
             int Total_Wage = Emp_Hr * Working_Hr;
-            Console.WriteLine("Total Employee Wage For Employee Is: " + Total_Wage);
+            Console.WriteLine("Total Emp Wage For Company " + Company + " Is: " + Total_Wage);
+            return Total_Wage;
 
         }
-
 
         static void Main(string[] args)
         {
-            Compute_Emp_Wages_Using_Class();
+            
+            Compute_Emp_Wages_Using_Class("Dmart", 20, 2, 10);
+            Compute_Emp_Wages_Using_Class("Reliance", 30, 4, 20);
         }
+
     }
+   
 }
